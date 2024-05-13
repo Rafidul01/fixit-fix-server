4316e161a71f85e34e190dd24815542fcb59c7f2
Rafidul01 <rafidulhasan555@gmail.com>
2024-05-14 05:17:20 +0600

    delete api added

--- index.js
+++ index.js
@@ -105,6 +105,15 @@ async function run() {
       const result = await servicesCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
+
+
+
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await servicesCollection.deleteOne(query);
+      res.send(result);
+    });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
