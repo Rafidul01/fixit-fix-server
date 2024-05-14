48c3d53727db24138581382f2b9c4af092a65d3c
Rafidul01 <rafidulhasan555@gmail.com>
2024-05-14 15:19:02 +0600

    Jwt token and api created

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 //middlewares--------
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true, // it is very important for send cookie to client
+  })
+);
+app.use(cookieParser());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hjxwn6k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -20,6 +28,27 @@ const client = new MongoClient(uri, {
   }
 });
 
+const logger = async (req, res, next) => {
+  console.log("called: ", req.method, req.url);
+  next();
+};
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log("tok tok token from middleware: ", token);
+  if (!token) {
+    return res.status(401).send({ message: "not authorized" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.error(err);
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+    console.log("value in the token : ", decoded);
+    req.user = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -27,9 +56,27 @@ async function run() {
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
+    app.post("/jwt", logger, async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      // create a token for user
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1hr",
+      });
+
+      // send cookie to client by http only
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
+
     const servicesCollection = client.db("fixitFix").collection("services");
     const bookedCollection = client.db("fixitFix").collection("booked");
-    app.get("/services", async (req, res) => {
+    app.get("/services", logger, verifyToken,  async (req, res) => {
       const filter = req?.query?.sort;
       const search = req?.query?.search;
       console.log(filter,search);
